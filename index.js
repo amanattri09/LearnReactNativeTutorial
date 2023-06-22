f2c9d31525f85394dea2e8196026a0ad49e27508
Aman Attri <aman.attri@assurant.com>
2023-06-22 21:39:39 +0530

    -added android permission tutorial

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import PermissionScreen from './src/permission_example/permission'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => PermissionScreen);
---
+++ src/permission_example/permission.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button, PermissionsAndroid, StatusBar, StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingTop: StatusBar.currentHeight,
+        backgroundColor: '#ecf0f1',
+        padding: 8,
+    },
+    item: {
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
+
+const requestRuntimePermissoin = async () => {
+    try{
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA,{
+            title : 'Cool Photo App Camera Permission',
+            message : 'Cool Photo App needs access to your camera ' +
+            'so you can take awesome pictures.',
+            buttonNeutral : 'Ask  Me Later',
+            buttonNegative : 'Cancel',
+            buttonPositive : 'Ok'
+        })
+        if(granted===PermissionsAndroid.RESULTS.GRANTED){
+            console.log('You can use the camera')
+        }else{
+            console.log('Camera permission denied')
+        }
+    }catch(error){
+        console.warn(error)
+    }
+}
+
+export default PermissionScreen = () => {
+    return (<View styles={styles.container}>
+        <Text style={styles.item}>Try permission</Text>
+        <Button title="request permissions" onPress={requestRuntimePermissoin}></Button>
+    </View>)
+}
