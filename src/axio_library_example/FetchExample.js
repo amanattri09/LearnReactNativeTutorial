e1428968d18f7629343e60cf866f45024afcad69
Aman Attri <aman.attri@assurant.com>
2023-06-23 13:17:27 +0530

    added code implemenation using axios

--- src/axio_library_example/FetchExample.js
+++ src/axio_library_example/FetchExample.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
+import axios from "axios";
 
 const styles = StyleSheet.create({
     container: {
@@ -16,27 +17,27 @@ const styles = StyleSheet.create({
 export default FetchExampleScreen = () => {
 
     const [data, setData] = useState([])
-    const [isLoading , setLoading]= useState(false)
+    const [isLoading, setLoading] = useState(false)
 
     function waitforme(millisec) {
         return new Promise(resolve => {
             setTimeout(() => { resolve('') }, millisec);
         })
     }
-      
+
 
     const getPostsAsync = async () => {
         console.log('on post async is working ....')
-        try{
+        try {
             setLoading(true)
-            await waitforme(5000) 
+            await waitforme(5000)
             const response = await fetch('https://jsonplaceholder.typicode.com/posts')
             const json = await response.json()
-            console.log('response'+json)    
+            console.log('response' + json)
             return json
-        }catch(error){
+        } catch (error) {
             console.error(error)
-        }finally{
+        } finally {
             setLoading(false)
         }
     }
@@ -44,27 +45,40 @@ export default FetchExampleScreen = () => {
     const getPostsUsingFetch = () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
-            .then(json => 
+            .then(json =>
                 setData(json)
             )
     }
 
+    
+
+    const getPostUsingAxios= async()=>{
+        axios({
+            method: 'get',
+            url: `https://jsonplaceholder.typicode.com/posts`,
+        }).then((response) => {
+            setData(response.data)
+        })
+    }
+
+
     useEffect(() => {
-        getPostsUsingFetch()
-         getPostsAsync().then(json=>{
-             setData(json)
-         })
+        // getPostsUsingFetch()
+        // getPostsAsync().then(json => {
+        //     setData(json)
+        // })
+         getPostUsingAxios()
     }, [])
 
     return (
         <View style={styles.container}>
             {
-                isLoading? (<ActivityIndicator/>) : (<FlatList data={data} keyExtractor={({id}) => id} renderItem={({item})=>(
-                         <View>
-                         <Text style={styles.item}>Id : {JSON.stringify(item.id)}</Text>
-                         <Text style={styles.item}>Body : {JSON.stringify(item.body)}</Text>
-                      </View>
-                     )}/>)
+                isLoading ? (<ActivityIndicator />) : (<FlatList data={data} keyExtractor={({ id }) => id} renderItem={({ item }) => (
+                    <View>
+                        <Text style={styles.item}>Id : {JSON.stringify(item.id)}</Text>
+                        <Text style={styles.item}>Body : {JSON.stringify(item.body)}</Text>
+                    </View>
+                )} />)
             }
         </View>
     )
